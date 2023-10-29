b135bba82023d5b7858c680cd1912950f63ba2e5
SipuliCodes <vepsalainensisu@gmail.com>
2023-10-29 15:57:44 +0200

    frontend liitetty backendiin

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('person', (req, res) => {
     return JSON.stringify(req.body)
